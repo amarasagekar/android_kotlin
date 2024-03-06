534a5cfc9442b4bc95092a6fb98b2e53f00c6f63
Amar Asagekar <amarasagekar@gmail.com>
2024-03-06 22:16:13 +0530

    Added Adapter

--- RecipeApp/app/src/main/java/com/jyotismita/recipeapp/HomeActivity.kt
+++ RecipeApp/app/src/main/java/com/jyotismita/recipeapp/HomeActivity.kt
@@ -2,10 +2,23 @@ package com.jyotismita.recipeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.jyotismita.recipeapp.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+    private lateinit var rvAdapter:PopularAdapter
+    private lateinit var dataList: ArrayList<Recipe>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setUpRecyclerview()
+    }
+
+    private fun setUpRecyclerview(){
+        dataList = ArrayList()
+        binding.rvPopular.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL, false)
     }
 }
--- RecipeApp/app/src/main/java/com/jyotismita/recipeapp/PopularAdapter.kt
+++ RecipeApp/app/src/main/java/com/jyotismita/recipeapp/PopularAdapter.kt
@@ -4,10 +4,12 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.jyotismita.recipeapp.databinding.PopularRvItemBinding
 
-class PopularAdapter(var dataList:ArrayList<Recipe>, var context: Context):RecyclerView.Adapter<PopularAdapter.ViewHolder> {
-    inner class ViewHolder(var binding: PopularRvItemBinding):RecyclerView.ViewHolder(binding.row)
+class PopularAdapter(var dataList:ArrayList<Recipe>, var context: Context):
+    RecyclerView.Adapter<PopularAdapter.ViewHolder>() {
+    inner class ViewHolder(var binding: PopularRvItemBinding):RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         var binding = PopularRvItemBinding.inflate(LayoutInflater.from(context), parent, false)
@@ -19,9 +21,11 @@ class PopularAdapter(var dataList:ArrayList<Recipe>, var context: Context):Recyc
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.binding.popularImg
+        Glide.with(context).load(dataList.get(position).img).into(holder.binding.popularImg)
         holder.binding.popularTxt.text=dataList.get(position).title
 
-        holder.binding.popularTime
+        var time = dataList.get(position).ing.split("\n".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+        holder.binding.popularTime.text = time.get(0)
+
     }
 }
